4cf79d71afcdd7bdbb2b56288e7542d73cc39844
Aditya <adityakhiroji7@gmail.com>
2026-04-07 10:27:25 +0000

    2nd

--- script.sh
+++ script.sh
@@ -4,10 +4,10 @@ echo "Step 1: Building Image"
 docker build -t mywebapp .
 
 echo "Step 2: Tagging Image"
-docker tag mywebapp <your-username>/mywebapp:latest
+docker tag mywebapp adityakhiroji/demo-rep1:Dockertest
 
 echo "Step 3: Pushing Image"
-docker push <your-username>/mywebapp:latest
+docker push adityakhiroji/demo-rep1:Dockertest
 
 echo "All steps completed!"
 
