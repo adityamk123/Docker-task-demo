fc0105bc173027e99cd0e2262a227a04b2bb4d7e
Aditya M Khiroji <adityakhiroji2@gmail.com>
2026-04-08 08:03:21 +0530

    Modify Docker commands in script.sh
    Updated Docker commands for building, tagging, and running the image with a new tag.

--- script.sh
+++ script.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 echo "Step 1: Building Image"
-docker build -t mywebapp .
+docker build -t adityakhiroji/demo-rep1:dockertest .
 
-echo "Step 2: Tagging Image"
-docker tag mywebapp adityakhiroji/demo-rep1:Dockertest
+#echo "Step 2: Tagging Image"
+#docker tag mywebapp adityakhiroji/demo-rep1:dockertest ;
 
 echo "Step 3: Pushing Image"
-docker push adityakhiroji/demo-rep1:Dockertest
+docker push adityakhiroji/demo-rep1:dockertest ;
 
 echo "Step 4: Stopping Old Container (if exists)..."
-docker stop webapp 2>/dev/null
-docker rm webapp 2>/dev/null
+#docker stop webapp 2>/dev/null
+docker rm -f webapp ;
+
+docker system prune -af ;
 
 echo "Step 5: Running New Container..."
-docker run -d -p 8080:80 --name webapp adityakhiroji/demo-rep1:Dockertest
+docker run -itd -p 8080:80 --name webapp adityakhiroji/demo-rep1:dockertest
 
